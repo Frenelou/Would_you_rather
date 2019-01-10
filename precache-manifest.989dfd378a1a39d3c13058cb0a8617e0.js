ab6e4d2d4eca037d87fcf6056362a77a84523ab1
Flora René-Louis-Arthur <flora.rene@egmail.com>
2019-01-10 17:35:49 +0100

    Updates

--- precache-manifest.989dfd378a1a39d3c13058cb0a8617e0.js
+++ precache-manifest.989dfd378a1a39d3c13058cb0a8617e0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Would_you_rather/static/js/runtime~main.82a32afb.js"
   },
   {
-    "revision": "9ac8e5dc14c9bca724bf",
-    "url": "/Would_you_rather/static/js/main.9ac8e5dc.chunk.js"
+    "revision": "4a853300fffff5dea64b",
+    "url": "/Would_you_rather/static/js/main.4a853300.chunk.js"
   },
   {
     "revision": "fde6134309a2f9e14649",
     "url": "/Would_you_rather/static/js/1.fde61343.chunk.js"
   },
   {
-    "revision": "9ac8e5dc14c9bca724bf",
+    "revision": "4a853300fffff5dea64b",
     "url": "/Would_you_rather/static/css/main.b61793a8.chunk.css"
   },
   {
-    "revision": "c9e66ecec401321fa68100f20e0df189",
+    "revision": "12ca95047d30f6ec0f11638705f4325b",
     "url": "/Would_you_rather/index.html"
   }
 ];
